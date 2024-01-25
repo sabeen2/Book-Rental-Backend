19a723ff3b0f3d4da1d5f3255777fe288c1f2d49
Bibekbhattarai00 <bibek.bhattarai83@gmail.com>
2024-01-25 13:02:10 +0545

    fix:cors filter added and fixed

--- src/main/java/com/example/bookrental/securityconfig/SecurityConfig.java
+++ src/main/java/com/example/bookrental/securityconfig/SecurityConfig.java
@@ -3,14 +3,12 @@ package com.example.bookrental.securityconfig;
 import com.example.bookrental.filter.JwtAuthFilter;
 import com.example.bookrental.repo.UserEntityRepo;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.filters.CorsFilter;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,11 +18,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
@@ -101,7 +97,7 @@ public class SecurityConfig {
 
         UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**",configuration);
-        return new CorsFilter();
+        return new CorsFilter(source);
     }
 
 
