8ceae2133b19268d8305f215fcd088ef3800c9f2
Bibekbhattarai00 <bibek.bhattarai83@gmail.com>
2024-03-17 16:02:15 +0545

    fix:coros added deployment

--- src/main/java/com/example/bookrental/securityconfig/SecurityConfig.java
+++ src/main/java/com/example/bookrental/securityconfig/SecurityConfig.java
@@ -97,7 +97,7 @@ public class SecurityConfig {
     public CorsFilter corsFilter(){
         CorsConfiguration configuration=new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://bookrental-7yd6.onrender.com"
-                , "https://book-rental-system-ts.netlify.app/", "https://bookrental-demo-production.up.railway.app"));
+                , "https://book-rental-system-ts.netlify.app/", "https://bookrental-demo-production.up.railway.app","https://book-rental.netlify.app/"));
         configuration.addAllowedHeader("*");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
